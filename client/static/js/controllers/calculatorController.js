7c1368ddd211536dc1edde1c7ccff7f025ac68bb
Pete Kang <kangpeter5@gmail.com>
2017-01-18 21:50:14 -0800

    almost... done....

--- client/static/js/controllers/calculatorController.js
+++ client/static/js/controllers/calculatorController.js
@@ -2,17 +2,22 @@ function calculatorController($scope){
 
 	// Sixteen oz
 	$scope.costPerCanSixteen = function(){
+		var costPerCanSixteen = 0;
 		var priceSixteen = $scope.priceSixteen;
 		var casesSixteen = $scope.casesSixteen;
 	
-		return priceSixteen/(24*casesSixteen);
+		costPerCanSixteen = (priceSixteen*casesSixteen)/(24*casesSixteen);
+
+		return costPerCanSixteen;
 	};
 
 	// Twenty oz
 	$scope.costPerCanTwenty = function(){
+		var costPerCanTwenty = 0;
 		var priceTwenty = $scope.priceTwenty;
 		var casesTwenty = $scope.casesTwenty;
 		
-		return priceTwenty/(24*casesTwenty);
+		costPerCanTwenty = (priceTwenty*casesTwenty)/(24*casesTwenty);
+		return costPerCanTwenty;
 	};
 };
